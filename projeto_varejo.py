b907eae8fe36f2c700d6a0dacf037e42e60c895d
thaisbarbosaf-26 <thais.barbosaf@gmail.com>
2026-08-16 14:09:04 -0300

    alteracao1 projeto

--- projeto_varejo.py
+++ projeto_varejo.py
@@ -19,6 +19,16 @@ df = df.dropna(axis=1, how="all")
 print("\n==================================================================================================================")
 print("\nQUANTIDADE DE VALORES VAZIOS POR COLUNA:") 
 print(df.isna().sum())
+print('\n')
+
+# Correção de valores inválidos na coluna 'DATA'
+datas_teste = pd.to_datetime(df['DATA'], format='%d/%m/%Y', errors='coerce')
+print('Datas inválidas:', datas_teste.isna().sum())
+
+# Alteração de valores inválidos em PR_CAT p/ 'SEM CATEGORIA'
+df['PR_CAT'] = df['PR_CAT'].replace('#N/D', 'SEM CATEGORIA')
+print(df['PR_CAT'].value_counts(dropna=False))
+
 
 # Salvar os dados tratados em um novo arquivo CSV
 df.to_csv("Base Varejo Tratada.csv", sep=";", index=False) 
